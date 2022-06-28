76f4d39ed15ca86b910404c74b6681b12110de54
aryannaf <aryannafarhadi@gmail.com>
2022-06-28 19:58:11 -0400

    Created explore page with navigation to categories

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import './App.scss';
 import HomePage from './pages/HomePage/HomePage';
 import Header from './components/Header/Header';
 import ExplorePage from './pages/ExplorePage/ExplorePage';
+import CategoriesPage from './pages/CategoriesPage/CategoriesPage';
+import AnimalDetailsPage from './pages/AnimalDetailsPage/AnimalDetailsPage';
+import AnimalListPage from './pages/AnimalListPage/AnimalListPage';
 
 function App() {
   return (
@@ -11,6 +14,9 @@ function App() {
       <Switch>
         <Route path="/" exact component={HomePage}/>
         <Route path="/explore" component={ExplorePage}/>
+        <Route path="/categories" component={CategoriesPage}/>
+        <Route path="/categories/:categoryId" component={AnimalListPage}/>
+        <Route path="/animal/:speciesId" component={AnimalDetailsPage}/>
       </Switch>
     </BrowserRouter>
   );
---
+++ src/pages/CategoriesPage/CategoriesPage.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './CategoriesPage.scss';
+
+function CategoriesPage() {
+  return (
+    <div>CategoriesPage</div>
+  )
+}
+
+export default CategoriesPage
