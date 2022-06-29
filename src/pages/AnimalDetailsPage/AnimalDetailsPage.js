b02bcd3b785932714878dd3c7b8e47d2f86e03d0
aryannaf <aryannafarhadi@gmail.com>
2022-06-29 13:12:42 -0400

    Created animal details page

--- src/pages/AnimalDetailsPage/AnimalDetailsPage.js
+++ src/pages/AnimalDetailsPage/AnimalDetailsPage.js
@@ -1,19 +1,74 @@
-import React from 'react';
+import { Component } from 'react';
 import './AnimalDetailsPage.scss';
-import { Canvas } from '@react-three/fiber';
-import { Suspense } from 'react';
+import axios from 'axios';
 import Landscape from '../../components/Landscape/Landscape';
 
-function AnimalDetailsPage() {
-    return (
-        <div>
-            <Canvas className="landscape">
-                <Suspense fallback={null}>
-                    <Landscape />
-                </Suspense>
-            </Canvas>
-        </div>
-    )
+
+const API_URL = "http://localhost:5050";
+
+class AnimalDetailsPage extends Component {
+
+    state = {
+        animalDetails: null
+    }
+
+    componentDidMount() {
+        axios
+            .get(`${API_URL}/species/${this.props.match.params.speciesId}`)
+            .then((response) => {
+                this.setState({
+                    animalDetails: response.data[0]
+                })
+            })
+            .catch(err => {
+                console.log(err)
+            })
+    }
+
+    render() {
+
+        const { animalDetails } = this.state;
+
+        if (!animalDetails) {
+            return <main>Loading, please wait...</main>;
+        }
+
+        return (
+            <main>
+                <div>
+                    <div>
+                        <h1>{animalDetails.name}</h1>
+                        <p>{animalDetails.scientificName}</p>
+                    </div>
+                    <div>
+                        <Landscape />
+                    </div>
+                    <div>
+                        <div>
+                            <p>{animalDetails.category}</p>
+                            <p>Population: {animalDetails.population}</p>
+                            <p>Habitat: {animalDetails.habitat}</p>
+                        </div>
+                        <div>
+                            <p>{animalDetails.information}</p>
+                            <p>Source: {animalDetails.source}</p>
+                        </div>
+                        <div>
+                            <h4>HOW TO HELP</h4>
+                            <div>
+                                <a href={animalDetails.petition}>
+                                    <button>PETITION</button>
+                                </a>
+                                <a href={animalDetails.donate}>
+                                    <button>DONATE</button>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </main>
+        )
+    }
 }
 
 export default AnimalDetailsPage
