1eeafed459255769b66ba8e27513af17c0b7d756
aryannaf <aryannafarhadi@gmail.com>
2022-07-02 17:50:51 -0400

    header styling - mobile

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,23 +1,26 @@
 import React from 'react';
 import './Header.scss';
 import { NavLink } from 'react-router-dom';
+import logo from '../../assets/images/logo.png';
 
 function Header() {
   return (
-    <div>
-      <div>
-        <NavLink to="/">
-          <h5>VR ANIMAL RESCUE</h5>
-        </NavLink>
+    <div className="header">
+      <div className="header__contents">
+        <div className="header__logo">
+          <NavLink to="/" className="header__logo-link">
+            <img src={logo} alt="vr animal rescue logo" className="header__logo-image" />
+          </NavLink>
+        </div>
+        <nav className="header__nav">
+          <NavLink to="/" className="header__nav-link">
+            <button className="header__about">ABOUT</button>
+          </NavLink>
+          <NavLink to="/explore" className="header__nav-link">
+            <button className="header__explore">EXPLORE</button>
+          </NavLink>
+        </nav>
       </div>
-      <nav>
-        <NavLink to="/">
-          <button>ABOUT</button>
-        </NavLink>
-        <NavLink to="/explore">
-          <button>EXPLORE</button>
-        </NavLink>
-      </nav>
     </div>
   )
 }
--- src/pages/AnimalDetailsPage/AnimalDetailsPage.js
+++ src/pages/AnimalDetailsPage/AnimalDetailsPage.js
@@ -1,7 +1,12 @@
 import { Component } from 'react';
 import './AnimalDetailsPage.scss';
 import axios from 'axios';
-import Landscape from '../../components/Landscape/Landscape';
+import Landscape1 from '../../components/Landscape1/Landscape1';
+import Landscape2 from '../../components/Landscape2/Landscape2';
+import { AdditiveAnimationBlendMode } from 'three';
+import AnimalListPage from '../AnimalListPage/AnimalListPage';
+
+import LandscapeRender from '../../components/LandscapeRender/LandscapeRender';
 
 
 const API_URL = "http://localhost:5050";
@@ -33,41 +38,54 @@ class AnimalDetailsPage extends Component {
             return <main>Loading, please wait...</main>;
         }
 
-        return (
-            <main>
-                <div>
-                    <div>
-                        <h1>{animalDetails.name}</h1>
-                        <p>{animalDetails.scientificName}</p>
-                    </div>
-                    <div>
-                        <Landscape />
-                    </div>
+        // const renderLandscape = () => {
+        //     if (animalDetails.id === 1) {
+        //         return <Landscape1 />
+        //     }
+        //     else {
+        //         return <Landscape2 />
+        //     }
+        // }
+
+        
+        
+
+            return (
+                <main>
                     <div>
                         <div>
-                            <p>{animalDetails.category}</p>
-                            <p>Population: {animalDetails.population}</p>
-                            <p>Habitat: {animalDetails.habitat}</p>
+                            <h1>{animalDetails.name}</h1>
+                            <p>{animalDetails.scientificName}</p>
+                        </div>
+                        <div className="animal__landscape">
+                            <LandscapeRender animal={animalDetails.id}/>
                         </div>
                         <div>
-                            <p>{animalDetails.information}</p>
-                            <p>Source: {animalDetails.source}</p>
-                        </div>
-                        <div>
-                            <h4>HOW TO HELP</h4>
                             <div>
-                                <a href={animalDetails.petition}>
-                                    <button>PETITION</button>
-                                </a>
-                                <a href={animalDetails.donate}>
-                                    <button>DONATE</button>
-                                </a>
+                                <p>{animalDetails.category}</p>
+                                <p>Population: {animalDetails.population}</p>
+                                <p>Habitat: {animalDetails.habitat}</p>
+                            </div>
+                            <div>
+                                <p>{animalDetails.information}</p>
+                                <p>Source: {animalDetails.source}</p>
+                            </div>
+                            <div>
+                                <h4>HOW TO HELP</h4>
+                                <div>
+                                    <a href={animalDetails.petition}>
+                                        <button>PETITION</button>
+                                    </a>
+                                    <a href={animalDetails.donate}>
+                                        <button>DONATE</button>
+                                    </a>
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
-            </main>
-        )
+                </main>
+            )
+
     }
 }
 
